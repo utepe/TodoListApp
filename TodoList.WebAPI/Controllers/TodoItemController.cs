014c2e6440f849cd46d5f3fee8ccca09aa712fdf
Uygur Tepe <utepe@ostusa.com>
2021-05-25 14:40:02 -0400

    tried to fix cors issue

--- TodoList.WebAPI/Controllers/TodoItemController.cs
+++ TodoList.WebAPI/Controllers/TodoItemController.cs
@@ -10,7 +10,7 @@ using TodoList.WebAPI.Models;
 
 namespace TodoList.WebAPI.Controllers
 {
-    [EnableCors(origins: "http://mywebclient.azurewebsites.net", headers: "*", methods: "*")]
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
     [Route("api/[controller]")]
     [ApiController]
     public class TodoItemController : ControllerBase
